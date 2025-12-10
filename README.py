0581fac8566fc3b6a24c60f3c333ce5ffd2ba7b3
kawtherkaddour <mathilda200012@gmail.com>
2025-12-10 22:43:27 +0100

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,8 @@
 # KaddourKawther-M1-MCQ
+# Microbiologie et controle de qualité... 10/12/2025
+# Membres de projet :
+#                   - Kaddour Kawther 
+#                   - Ilias Amel
+#                   - Guennoun Amel
+#                   - Nmiche Imane
+#                   - Zemmour Khadidja 
