3834d7de01c17ff2c5ba45964eb9cbda7534934a
kawtherkaddour <mathilda200012@gmail.com>
2025-12-11 21:16:55 +0100

    Update Kaddour Kawther M1 MCQ.py

--- Kaddour Kawther M1 MCQ.py
+++ Kaddour Kawther M1 MCQ.py
@@ -6,4 +6,15 @@
  #                    - Guennoun Amel 
  #                    - Nmiche Imane 
  #                    - Zemmour Khadidja 
- 
+ import pandas as pd 
+# Données : Séquences ADN, Longueur, Porcentage de GC
+data = { 
+"Séquence": ["ATGCGTACGTA","GCTAGCTAGGCC","ATGCGCGTAAGT","TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"],
+ "Longueur":[12,12,12,10,11,10,10],"Porcentage de GC":[50,66.67,58.33,40,45.45,60,50]
+ } 
+# Création d'un Dataframe ( Tableau pandas )
+df= pd.Dataframe(data)
+print("******************** Création et Affichage ********************") 
+# Affichage de tableau 
+print("Tableau des séquence d'ADN:")
+print(df)
