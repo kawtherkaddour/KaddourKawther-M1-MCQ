58adc2c86aa25a4826ebd91a0412436b0e6977a6
kawtherkaddour <mathilda200012@gmail.com>
2025-12-11 21:04:02 +0100

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,9 @@
+# Kaddour Kawther 
+ # Microbiologie et controle de qualité... 11/12/2025
+ # Membres de projet :
+ #                    - Kawther Kaddour 
+ #                    - Ilias Amel 
+ #                    - Guennoun Amel 
+ #                    - Nmiche Imane 
+ #                    - Zemmour Khadidja 
+ 
