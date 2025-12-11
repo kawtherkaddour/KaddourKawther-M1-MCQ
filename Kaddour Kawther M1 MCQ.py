5080c3c67751f6c0b3f1b01bd4e5d9782a8d08ea
khadidjazemmour42-droid <khadidjazemmour42@gmail.com>
2025-12-12 00:21:21 +0100

    Update Kaddour Kawther M1 MCQ.py

--- Kaddour Kawther M1 MCQ.py
+++ Kaddour Kawther M1 MCQ.py
@@ -37,3 +37,7 @@ print("********************Calcul de la moyenne********************")
 average_gc = df["Pourcentage GC"].mean()
 print(f"Pourcentage moyen de GC: {average _gc:.3f}%","\n\n")
 
+#5 ) Ajouter une nouvelle colonne avec des calculs
+Print("********** Ajouter d'une nouvelle colonne **********")
+# Ajouter une nouvelle colonne " Catégorie GC "
+df["Catégorie GC"] = df["Pourcentage GC"] .apply(lambda x: "Rich" if x > 55 else "Moyen" if 45 ≤ x ≥ 55 else "Faible" if x < 45)
